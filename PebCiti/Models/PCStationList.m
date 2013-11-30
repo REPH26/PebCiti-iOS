bf01d985ababeb7271cedf5057ed549b37d904c8
REPH26 <me@reph.fr>
2013-11-30 11:38:08 -0800

    Adding French source for bike

--- PebCiti/Models/PCStationList.m
+++ PebCiti/Models/PCStationList.m
@@ -22,7 +22,7 @@
 
 - (void)requestStationList
 {
-    NSURL *URL = [NSURL URLWithString:@"http://citibikenyc.com/stations/json"];
+    NSURL *URL = [NSURL URLWithString:@"https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=51c05c8d85aa71cfea75406761eef6b7f4a488c7"];
     NSURLRequest *request = [NSURLRequest requestWithURL:URL];
     NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
     [connection start];
@@ -34,7 +34,7 @@
     if (!userLocation) {
         return nil;
     }
-
+    
     [self sortStationsByDistance];
     NSPredicate *availableBikesPredicate = [NSPredicate predicateWithFormat:@"bikesAvailable > 0"];
     return [self.stations filteredArrayUsingPredicate:availableBikesPredicate][0];
@@ -46,7 +46,7 @@
     if (!userLocation) {
         return nil;
     }
-
+    
     [self sortStationsByDistance];
     NSPredicate *openDocksPredicate = [NSPredicate predicateWithFormat:@"docksAvailable > 0"];
     return [self.stations filteredArrayUsingPredicate:openDocksPredicate][0];
@@ -68,25 +68,38 @@
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
-    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:self.data options:0 error:nil];
-    NSArray *stationInfos = json[@"stationBeanList"];
-
+    
+    NSError *e = nil;
+    NSArray *json = [NSJSONSerialization JSONObjectWithData:self.data options: NSJSONReadingMutableContainers error: &e];
+    
+    if (!json) {
+        NSLog(@"Error parsing JSON: %@", e);
+    } else {
+        for(NSDictionary *item in json) {
+            NSLog(@"Item: %@", item);
+        }
+    }
+    NSArray *stationInfos = json;
+    
     if (!stationInfos) {
         [UIAlertView displayAlertViewWithTitle:@"" message:@"A problem occurred downloading the station list from citibikenyc.com"];
     } else {
         NSMutableArray *stations = [@[] mutableCopy];
         PCStation *station;
         for (NSDictionary *stationInfo in stationInfos) {
-            station = [[PCStation alloc] initWithID:stationInfo[@"id"]];
-            station.name = stationInfo[@"stationName"];
-            station.bikesAvailable = [stationInfo[@"availableBikes"] integerValue];
-            station.docksAvailable = [stationInfo[@"availableDocks"] integerValue];
-            station.location = [[CLLocation alloc] initWithLatitude:[stationInfo[@"latitude"] floatValue] longitude:[stationInfo[@"longitude"] floatValue]];
+            station = [[PCStation alloc] initWithID:stationInfo[@"number"]];
+            station.name = stationInfo[@"name"];
+            station.bikesAvailable = [stationInfo[@"available_bikes"] integerValue];
+            station.docksAvailable = [stationInfo[@"available_bike_stands"] integerValue];
+            station.location = [[CLLocation alloc] initWithLatitude:[stationInfo[@"position"][@"lat"]  floatValue] longitude:[stationInfo[@"position"][@"lng"] floatValue]];
+            NSLog(@"Station: %@", station);
             [stations addObject:station];
+            NSLog(@"Stations: %@", stations);
         }
+        NSLog(@"Item: %@", stations);
         self.stations = stations;
         [self sortStationsByDistance];
-
+        
         [self.delegate stationListWasUpdated:self];
         self.data = [[NSMutableData alloc] init];
         [self performSelector:@selector(requestStationList) withObject:nil afterDelay:30];
@@ -102,7 +115,7 @@
             CLLocation *userLocation = PebCiti.sharedInstance.locationManager.location;
             CLLocationDistance station1Distance = [userLocation distanceFromLocation:station1.location];
             CLLocationDistance station2Distance = [userLocation distanceFromLocation:station2.location];
-
+            
             if (station1Distance < station2Distance) {
                 return NSOrderedAscending;
             } else if (station1Distance > station2Distance) {
