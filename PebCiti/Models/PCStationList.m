512b0bca250da63bdfd4c441c538f584ddd4741d
REPH26 <me@reph.fr>
2013-12-05 15:11:23 +0100

    Update PCStationList.m
    Cleaning

--- PebCiti/Models/PCStationList.m
+++ PebCiti/Models/PCStationList.m
@@ -19,7 +19,6 @@
     }
     return self;
 }
-
 - (void)requestStationList
 {
     NSURL *URL = [NSURL URLWithString:@"https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=51c05c8d85aa71cfea75406761eef6b7f4a488c7"];
@@ -27,14 +26,12 @@
     NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO];
     [connection start];
 }
-
 - (PCStation *)closestStationWithAvailableBike
 {
     CLLocation *userLocation = PebCiti.sharedInstance.locationManager.location;
     if (!userLocation) {
         return nil;
     }
-    
     [self sortStationsByDistance];
     NSPredicate *availableBikesPredicate = [NSPredicate predicateWithFormat:@"bikesAvailable > 0"];
     return [self.stations filteredArrayUsingPredicate:availableBikesPredicate][0];
@@ -46,41 +43,25 @@
     if (!userLocation) {
         return nil;
     }
-    
     [self sortStationsByDistance];
     NSPredicate *openDocksPredicate = [NSPredicate predicateWithFormat:@"docksAvailable > 0"];
     return [self.stations filteredArrayUsingPredicate:openDocksPredicate][0];
 }
-
 #pragma mark - <NSURLConnectionDelegate>
-
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
 {
     [UIAlertView displayAlertViewWithTitle:@"" message:@"A problem occurred downloading the station list from JCDecaux"];
 }
-
 #pragma mark - <NSURLConnectionDataDelegate>
-
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
 {
     [self.data appendData:data];
 }
-
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
-    
     NSError *e = nil;
     NSArray *json = [NSJSONSerialization JSONObjectWithData:self.data options: NSJSONReadingMutableContainers error: &e];
-    
-    if (!json) {
-        NSLog(@"Error parsing JSON: %@", e);
-    } else {
-        for(NSDictionary *item in json) {
-            NSLog(@"Item: %@", item);
-        }
-    }
     NSArray *stationInfos = json;
-    
     if (!stationInfos) {
         [UIAlertView displayAlertViewWithTitle:@"" message:@"A problem occurred downloading the station list from JCDecaux"];
     } else {
@@ -92,11 +73,8 @@
             station.bikesAvailable = [stationInfo[@"available_bikes"] integerValue];
             station.docksAvailable = [stationInfo[@"available_bike_stands"] integerValue];
             station.location = [[CLLocation alloc] initWithLatitude:[stationInfo[@"position"][@"lat"]  floatValue] longitude:[stationInfo[@"position"][@"lng"] floatValue]];
-            NSLog(@"Station: %@", station);
             [stations addObject:station];
-            NSLog(@"Stations: %@", stations);
         }
-        NSLog(@"Item: %@", stations);
         self.stations = stations;
         [self sortStationsByDistance];
         
@@ -105,9 +83,7 @@
         [self performSelector:@selector(requestStationList) withObject:nil afterDelay:30];
     }
 }
-
 #pragma mark - Private
-
 - (void)sortStationsByDistance
 {
     if (PebCiti.sharedInstance.locationManager.location) {
@@ -115,7 +91,6 @@
             CLLocation *userLocation = PebCiti.sharedInstance.locationManager.location;
             CLLocationDistance station1Distance = [userLocation distanceFromLocation:station1.location];
             CLLocationDistance station2Distance = [userLocation distanceFromLocation:station2.location];
-            
             if (station1Distance < station2Distance) {
                 return NSOrderedAscending;
             } else if (station1Distance > station2Distance) {
@@ -126,5 +101,4 @@
         }];
     }
 }
-
 @end
